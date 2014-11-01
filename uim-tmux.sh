d7f8ef098165e55d0c750ec47af07d2041f98cb1
NgoHuy <huynhok.uit@gmail.com>
2014-11-02 00:48:40 +0700

    Refactor scripts

--- uim-tmux.sh
+++ uim-tmux.sh
@@ -3,15 +3,18 @@
 # Purpose: input method for each tmux window
 # License: GPLv3 (http://www.gnu.org/copyleft/gpl.html)
 
-window=$(tmux display-message -p '#I')
+window=$(tmux display-message -p '#W') # get window name
 lockfile="/tmp/uim.lock"
 
-if [[ $window -eq 0 ]] ; then
-  [[ -f $lockfile ]] && exit 0
-  touch $lockfile 
-  xdotool key ctrl+space
-elif [[ $window -gt 0 ]] && [[ -f $lockfile ]]; then
-  rm $lockfile 
-  xdotool key ctrl+space
+touch $lockfile
+
+if [[ "$window" == "irc" ]] ; then
+  uim_on=$(cat $lockfile)
+  [[ $uim_on -eq 1 ]] && exit 0
+  echo 1 > $lockfile && xdotool key ctrl+space
+else
+  uim_on=$(cat $lockfile)
+  [[ $uim_on -eq 0 ]] && exit 0 
+  echo 0 > $lockfile && xdotool key ctrl+space
 fi
   
